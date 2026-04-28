0415ac4fc13fbe938fc75019b29cf6ff353b8a9c
Saigoutham2005 <goutham_musanam@srmap.edu.in>
2026-04-28 17:39:23 +0530

    Force remove folder

--- CivicConnect-master/backend/models/Department.js
+++
@@ -1,9 +0,0 @@
-// models/Department.js
-const mongoose = require('mongoose');
-
-const deptSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    officers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Department', deptSchema);
